7f877d3e7d3c08505c6790c9c62de19e90a142ba
MoeinGanji2568 <146331540+MoeinGanji2568@users.noreply.github.com>
2025-02-16 20:49:21 +0330

    update: update page to jsx and import component for landing

---
+++ my-portfolio/src/app/page.jsx
@@ -0,0 +1,13 @@
+import AboutSection from "@/components/landing/AboutSection";
+import HeroSection from "@/components/landing/HeroSection";
+import SkillsSection from "@/components/landing/SkillsSection";
+
+export default function Home() {
+  return (
+    <>
+      <HeroSection />
+      <AboutSection />
+      <SkillsSection />
+    </>
+  );
+}
