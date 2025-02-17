0fe83b86b82d65f94fa668b75603b27b6a197e7b
MoeinGanji2568 <146331540+MoeinGanji2568@users.noreply.github.com>
2025-02-17 16:19:25 +0330

    add: icons object for images url

---
+++ my-portfolio/src/utils/icon.js
@@ -0,0 +1,15 @@
+const techIcons = {
+  htmlIcon: "/image/icon/html5.svg",
+  cssIcon: "/image/icon/css3.png",
+  jsIcon: "/image/icon/js.png",
+  tsIcon: "/image/icon/typeScript.svg",
+  reactIcon: "/image/icon/react.png",
+  nextIcon: "/image/icon/next.png",
+  viteIcon: "/image/icon/Vite.png",
+  tailwindIcon: "/image/icon/tailwind.svg",
+  muiIcon: "/image/icon/MUI.png",
+  figmaIcon: "/image/icon/figma.svg",
+  gitIcon: "/image/icon/git.png",
+};
+
+export default techIcons;
