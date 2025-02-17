7e22efa6f3f0f64bfcffa324d00500368eb76ae5
MoeinGanji2568 <146331540+MoeinGanji2568@users.noreply.github.com>
2025-02-17 23:37:20 +0330

    add: experience json at data

---
+++ my-portfolio/src/constants/data/experience.js
@@ -0,0 +1,74 @@
+export const experience = [
+  {
+    id: 1,
+    label: "Sepehr Academy",
+    duration: "2 yr 1 mos",
+    position: "React.js | Next.js | Mentor",
+    type: "Full-time",
+    more: "August 2022 — September 2024 . Sari, Mazandaran Province, Iran . On-site / Hybrid",
+    description: [
+      {
+        paragraph:
+          "Optimized web pages for maximum speed and scalability, reducing load times by 25% through techniques like lazy-loading.",
+      },
+      {
+        paragraph:
+          " Built responsive web applications with React, ensuring cross-browser compatibility and mobile optimization.",
+      },
+      {
+        paragraph:
+          "Implemented state management using Redux Toolkit in a multi-component React app to enhance performance and code maintainability.",
+      },
+      {
+        paragraph:
+          "Collaborated with a team of developers to deliver robust React applications, adhering to project deadlines.",
+      },
+      {
+        paragraph:
+          " Mentored 3 development groups, providing guidance on HTML, CSS, and JavaScript fundamentals.",
+      },
+      {
+        paragraph:
+          "Enhanced SEO and accessibility using Next.js features like server-side rendering (SSR) and static site generation (SSG), leading to a 25% increase in organic traffic.",
+      },
+      {
+        paragraph:
+          " Led a team of junior developers, providing mentorship and code reviews to ensure adherence to coding standards and best practices.",
+      },
+    ],
+  },
+  {
+    id: 2,
+    label: "Arman Pajohan Science and Technology Company (Apanco)",
+    duration: "1 yr",
+    position: "React.js",
+    type: "Full-time",
+    more: "July 2021 — July 2022 . Sari, Mazandaran Province, Iran . Remote / Hybrid",
+    description: [
+      {
+        paragraph:
+          " Developed an educational web app using React, integrating various APIs for real-time data fetching and error handling.",
+      },
+      {
+        paragraph:
+          "Utilized React Hooks and functional components to eliminate redundancy and improve efficiency.",
+      },
+      {
+        paragraph:
+          "Developed reusable components and design systems with Storybook and TypeScript, improving code consistency across multiple projects.",
+      },
+      {
+        paragraph:
+          " Implemented multilingual support using next-i18next and next-translate, enabling seamless localization for global audiences.",
+      },
+      {
+        paragraph:
+          " Refactored legacy codebase to improve readability, maintainability, and performance, reducing technical debt and improving development velocity.",
+      },
+      {
+        paragraph:
+          "Created custom hooks and utilities to abstract complex logic, promoting code reuse and reducing redundancy across multiple components.",
+      },
+    ],
+  },
+];
