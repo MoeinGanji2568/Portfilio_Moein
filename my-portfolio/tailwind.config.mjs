1923cc66d4644f7e2291d081eb2fa1c0e8172eab
MoeinGanji2568 <146331540+MoeinGanji2568@users.noreply.github.com>
2025-02-18 19:17:22 +0330

    update: change content address in tailwind config

--- my-portfolio/tailwind.config.mjs
+++ my-portfolio/tailwind.config.mjs
@@ -12,7 +12,12 @@ function withOpacity(variableName) {
 }
 
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   darkMode: ["class", '[class="dark-mode"]'],
   theme: {
     extend: {
