b37fb7bccff54d21038fbc2e21c4332558f7fe4e
MoeinGanji2568 <146331540+MoeinGanji2568@users.noreply.github.com>
2025-02-19 19:13:11 +0330

    add: add my skills list in data folder

---
+++ my-portfolio/src/constants/data/mySkills.js
@@ -0,0 +1,16 @@
+import techIcons from "@/utils/icon";
+
+const MySkills = [
+  { id: 1, label: "HTML5", icon: techIcons.htmlIcon },
+  { id: 2, label: "CSS3", icon: techIcons.cssIcon },
+  { id: 3, label: "JavaScript", icon: techIcons.jsIcon },
+  { id: 4, label: "TypeScript", icon: techIcons.tsIcon },
+  { id: 5, label: "React.js", icon: techIcons.reactIcon },
+  { id: 6, label: "Next.js", icon: techIcons.nextIcon },
+  { id: 7, label: "Vite", icon: techIcons.viteIcon },
+  { id: 8, label: "Tailwind CSS", icon: techIcons.tailwindIcon },
+  { id: 9, label: "Material UI", icon: techIcons.muiIcon },
+  { id: 10, label: "Figma", icon: techIcons.figmaIcon },
+  { id: 11, label: "Git & GitHub", icon: techIcons.gitIcon },
+];
+export default MySkills;
